a408b862e0458695330fcbfee68d47d3f32ba3a1
NingerJohn <ningerjohn@163.com>
2015-10-27 16:26:55 +0800

    update on 2015-10-27

---
+++ frontend/controllers/EntryController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* 网站入口控制器
+*/
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
